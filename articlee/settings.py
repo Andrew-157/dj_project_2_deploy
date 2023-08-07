ddd2b4b21c308578f76b9cac2fa4558b58d83806
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-07 20:25:24 +0200

    Update settings.py

--- articlee/settings.py
+++ articlee/settings.py
@@ -35,7 +35,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['web-production-67a3.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-67a3.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-67a3.up.railway.app']
 
 # Application definition
 
