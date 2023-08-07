f004b250d4956ed1a3fda811d50c1801868a9b15
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-07 19:54:20 +0200

    Update settings.py

--- articlee/settings.py
+++ articlee/settings.py
@@ -96,15 +96,10 @@ WSGI_APPLICATION = 'articlee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("DB_NAME"),
-        'HOST': os.environ.get("DB_HOST"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'PORT': os.environ.get("DB_PORT")
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
